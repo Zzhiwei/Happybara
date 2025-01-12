28ad2f87134c795d99ea2e8fab41211c5306bb54
Zzhiwei <zhiwei3141@gmail.com>
2025-01-12 18:18:50 +0800

    [refactor command] refactors remind, list, delete commands

--- app/services/telegram_bot/telegram_bot_service.rb
+++ app/services/telegram_bot/telegram_bot_service.rb
@@ -1,5 +1,3 @@
-require 'byebug'
-
 module TelegramBot
   class TelegramBotService
     def run
@@ -55,75 +53,15 @@ module TelegramBot
       when "/new"
         Commands::NewCommand.new(@bot, message, user).execute
       when "/list"
-        maxListed = 10
-        expensesArray = Expense.where("user_id LIKE ?", "%#{user.id}%").order(id: :desc)
-        responseString = "Here is your list of expenses!\n\n"
-        totalAmount = 0
-        expensesArray.each_with_index do |expense, i|
-          if i < maxListed
-            expenseString = "#{i+1}: #{expense.title} - $#{expense.amount}"
-            expense.tags.each do |tag|
-              expenseString << " ##{tag.name}"
-            end
-            responseString << expenseString + "\n"
-          end
-          totalAmount += expense.amount
-        end
-        if expensesArray.length > maxListed
-          responseString << "...\n"
-        end
-        responseString << "\nTotal amount spent: $#{totalAmount}"
-        @bot.api.send_message(chat_id: message.chat.id, text: responseString)
+        Commands::ListCommand.new(@bot, message, user).execute
       when "/delete"
-        lastExpense = Expense.where("user_id LIKE ?", "%#{user.id}%").order(id: :desc).first
-        if lastExpense
-          lastExpense.destroy
-          @bot.api.send_message(chat_id: message.chat.id, text: "Last expense deleted: #{lastExpense.title}, $#{lastExpense.amount}")
-        end
+        Commands::DeleteCommand.new(@bot, message, user).execute
       when "/remind"
-          handle_reminder_commands(user.id, message.chat.id, content)
+        Commands::RemindCommand.new(@bot, message, user).execute
       when "/tag"
         handle_tag_commands(user, message.chat.id, content)
       else
         handle_unknown_command(message.chat.id)
-      end
-    end
-
-    def handle_reminder_commands(user_id, chat_id, content)
-      if content.nil?
-        @bot.api.send_message(chat_id: chat_id, text: "The /remind commands are:\ncreate [hour]\ndelete [hour]\nlist")
-      elsif content.include? ("create")
-        _, hour = content.split(" ", 2)
-        result = TelegramRemindersService.create_reminder(user_id: user_id, hour: Integer(hour))
-        if result.key?(:success)
-          @bot.api.send_message(chat_id: chat_id, text: "New reminder created for #{hour}!")
-        else
-          @bot.api.send_message(chat_id: chat_id, text: "Sorry, please try again. #{result.error}")
-        end
-      elsif content.include? ("delete")
-        _, hour = content.split(" ", 2)
-        result = TelegramRemindersService.delete_reminder(user_id: user_id, hour: hour)
-        if result.key?(:success)
-          @bot.api.send_message(chat_id: chat_id, text: "Reminder deleted for #{hour}!")
-        else
-          @bot.api.send_message(chat_id: chat_id, text: "Sorry, please try again. #{result.error}")
-        end
-      elsif content.include? ("list")
-        puts "remind list"
-        maxListed = 10
-        remindersArray = TelegramRemindersService.get_reminders_for_user_id(user_id)
-        responseString = "Here is your list of reminders!\n\n"
-        remindersArray.each_with_index do |reminder, i|
-          if i < maxListed
-            responseString << "#{i+1}: #{reminder}\n"
-          end
-        end
-        if remindersArray.length > maxListed
-          responseString << "...\n"
-        end
-        @bot.api.send_message(chat_id: chat_id, text: responseString)
-      else
-        @bot.api.send_message(chat_id: chat_id, text: "The /remind commands are:\ncreate [hour]\ndelete [hour]\nlist")
       end
     end
 
